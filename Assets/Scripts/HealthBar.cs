3da4f19e739218b378946f2dd506db46419a3d1c
IlanRepiso <101400193+IlanRepiso@users.noreply.github.com>
2022-06-01 14:41:48 -0300

    Health and Control Script Separated

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HealthBar : MonoBehaviour
+{
+    bool isTakingDmg;
+    bool isAlive;
+    int healthBar = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTakingDmg = false;
+        isAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTakingDmg)
+        {
+            healthBar -= 1;
+        }
+        if (healthBar > 0)
+        {
+            isAlive = true;
+        }
+        if (healthBar < 1)
+        {
+            isAlive = false;
+        }
+        if (isAlive == false)
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Enemy")
+        {
+            isTakingDmg = true;
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.name == "Enemy")
+        {
+            isTakingDmg = false;
+        }
+    }
+}
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     float PlayerSpeed = 0.5f;
     float PlayerRotation = 7.5f;
-    bool isTakingDmg;
-    bool isAlive;
-    public int HealthBar = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        isTakingDmg = false;
-        isAlive = true;
+        
     }
 
     // Update is called once per frame
@@ -23,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-        transform.Translate(0, 0, PlayerSpeed);
+            transform.Translate(0, 0, PlayerSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -36,36 +32,6 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.eulerAngles += new Vector3(0, -PlayerRotation, 0);
-        }
-        if (isTakingDmg)
-        {
-            HealthBar -= 1;
-        }
-        if (HealthBar > 0)
-        {
-            isAlive = true;
-        }
-        if (HealthBar < 1)
-        {
-            isAlive = false;
-        }
-        if (isAlive == false)
-        {
-            SceneManager.LoadScene("Gameplay");
-        }
-    }
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.name == "Enemy")
-        {
-            isTakingDmg = true;
-        }
-    }
-    void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.name == "Enemy")
-        {
-            isTakingDmg = false;
         }
     }
 }
